acbd1c2e7e5b052d9b3747d4c50e4efe1addb4fc
MichelleGathii <michygathii@gmail.com>
2023-08-11 07:07:26 +0300

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,5 @@ float f;
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %lld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %zu byte(s)\n", sizeof(f));
-		return(0);
+		return (0);
 }
