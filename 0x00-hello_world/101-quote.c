30696342c04f91593340a4c12d3c28615d195366
MichelleGathii <michygathii@gmail.com>
2023-08-11 09:59:04 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<unistd.h>
 /**
  * main - Entry point
  *
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-
-    return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 
