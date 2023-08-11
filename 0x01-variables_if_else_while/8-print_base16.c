03d630509bfbfc7dcd48d0b066c9ac77c23bbeeb
MichelleGathii <michygathii@gmail.com>
2023-08-12 00:14:07 +0300

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+  *main - entry point. The function prints numbers of base 9
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i = 0;
+	char c = 'a';
+
+	while (i <= 9)
+	{
+		putchar(i + 48);
+		i++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+return (0);
+}
