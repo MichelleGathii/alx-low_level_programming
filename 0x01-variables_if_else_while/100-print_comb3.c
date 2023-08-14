5e258489c008be71978244bccc2f7586c18b9344
MichelleGathii <michygathii@gmail.com>
2023-08-14 08:47:23 +0300

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-  *main - prints all possible combinations of two numbers without repetitio 
+  *main - prints all possible combinations of two numbers without repetition
   *Return: Always 0 (Success)
   */
 int main(void)
@@ -9,13 +9,13 @@ int main(void)
 	int j;
 
 	while (i <= 9)
-	{	
+	{
 		j = i + 1;
 		while (j <= 9)
 		{
 			putchar(i + 48);
 			putchar(j + 48);
-			if (i == 8 && j == 9 )
+			if (i == 8 && j == 9)
 			{
 				putchar('\n');
 			}
