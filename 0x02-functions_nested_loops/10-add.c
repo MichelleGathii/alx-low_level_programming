8667bb09b845ef5865c2d2dfcd161877b795f9e1
MichelleGathii <michygathii@gmail.com>
2023-08-15 21:35:07 +0300

    Betty 10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- * main - check the code
- *
+ * add - adds two numbers
+ * @m: 1st number to add
+ * @n: 2nd number to add
  * Return: Always 0.
  */
 int add(int m, int n)
 {
 	int sum;
+
 	sum = m + n;
 return (sum);
 }
