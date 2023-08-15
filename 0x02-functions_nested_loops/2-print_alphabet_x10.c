0b061b53bef5e8d08c1f464f22bf4e9b53fd351d
MichelleGathii <michygathii@gmail.com>
2023-08-15 14:25:27 +0300

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,20 @@
   */
 void print_alphabet_x10(void)
 {
-	char l = 'a';
-	int i =0;
+	int i = 0;
 	
 	while (i <= 10)
 	{
+		char l = 'a';
+
 		while (l <= 'z')
 		{
 			_putchar(l);
 			l++;
 		}
+	_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
+
+
