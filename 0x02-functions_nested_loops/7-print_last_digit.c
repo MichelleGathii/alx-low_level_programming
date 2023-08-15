14bb187cc5060a46ed525f4ab0162aa16ccc16a8
MichelleGathii <michygathii@gmail.com>
2023-08-15 21:24:26 +0300

    Betty 7-last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_last_digit - check the code
+ * @i: number to be checked for lastdigit
+ * Return: the last digit
  */
 int print_last_digit(int i)
 {
 	int l = 0;
+
 	l = i % 10;
 	if (l < 0)
 		l *= -1;
