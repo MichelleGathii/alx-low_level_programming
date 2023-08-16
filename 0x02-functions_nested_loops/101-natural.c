1173f6fe03e0bb8e2a04b6ea7320c8e90957b85b
MichelleGathii <michygathii@gmail.com>
2023-08-16 05:39:07 +0300

    100-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  * main - prints the sum of numbers divisible by 3 and 5
+  * Return: sum
+  *
+  */
+int main(void)
+{
+	int i = 0;
+	int sum = 0;
+
+	while (i < 1024)
+	{
+		if (i % 5 == 0 || i % 3 == 0)
+		{
+			sum += i;
+			i++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	printf("%d", sum);
+	return (sum);
+}
