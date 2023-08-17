c6c6923c19585b0a9f3d48fb0eb7425e3c811c4a
MichelleGathii <michygathii@gmail.com>
2023-08-17 15:40:29 +0300

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c <= 90 && c >= 41 )
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
