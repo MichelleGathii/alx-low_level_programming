f9353805721916e397c86dbf2bc0c1451a4ce2b3
MichelleGathii <michygathii@gmail.com>
2023-08-17 11:26:21 +0300

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+#include "main.h"
 /**
   *main - prints fibonacci numbers
   *Return: Always 0 (Success)
   */
-int fibonacci(void);
 
 int main(void)
 {
@@ -11,20 +11,30 @@ int main(void)
 	return (0);
 }
 /**
-  *main - prints fibonacci numbers
-  *
+  *fibonacci - prints fibonacci numbers
+  *Return: always 0 Succecc
   */
 int fibonacci(void)
 {
-	int i = 0;
-	int sum = 0;
+	long i = 1;
+	long j = 2;
+	long fib = 0;
+	int n;
 
-	while (i <= 25)
+	printf("%ld, %ld, ", i, j);
+	n = 0;
+	while (n <= 48)
 	{
-		sum += i;
-		printf("%d, ", sum);
-		i++;
+		fib = i + j;
+		printf("%ld", fib);
+		if (n != 48)
+			printf(", ");
+		else
+			printf("\n");
+		i = j;
+		j = fib;
+	n++;
 	}
 	printf("\n");
-return (sum);
+return (0);
 }
