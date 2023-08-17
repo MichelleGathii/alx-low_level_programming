caff7ff136e066576da5ba8a4c9c474bac5753f1
MichelleGathii <michygathii@gmail.com>
2023-08-17 09:15:32 +0300

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
- *
+ * print_to_98 - prints numbers to 98
+ *@n:  the number to be checked
  * Return: Always 0.
  */
 void print_to_98(int n)
